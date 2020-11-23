fc4f7306d558fcdbf1d867b0a1471f87dad980ec
Olamide Adekoya <adekoyaolamide643@gmail.com>
2020-11-23 10:39:44 +0100

    make changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize , sent_tokenize 
 import heapq
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import pickle 
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
@@ -14,8 +15,9 @@ model = load_model("Sentiment_Analysis_on_TR")
 with open('tokenize.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/', methods = ['POST'])
+@cross_origin(origin = "*")
 def classify():
     Request = request.get_json()
     url = Request['url']
@@ -43,12 +45,11 @@ def classify():
         Date_created = blog['createdAt'][0:10]
         Time_Created = blog['createdAt'][11:19]
         Last_Updated = blog['updatedAt'][0:10]
-        Lime_Updated = blog['updatedAt'][11:19]
+        Time_Updated = blog['updatedAt'][11:19]
 
     
         text_ = content.lower()
-        stop = {'under', 'do', 'because', "don't", 'with', 'can', 'mustn', 'our', 'am', "should've", 'over', 'few', 'yourself', 'what', 's', "hasn't", 'were', 'now', "weren't", 'to', "you're", 'd', 'some', 'being', 'the', "haven't", 'wasn', "aren't", "couldn't", "wouldn't", 'where', 'don', 'having', 'so', 'at', 'hasn', 'is', 'more', 'only', 'same', "needn't", 'an', 'most', 'very', 're', 'who', 'not', "she's", 'herself', 'you', 'them', 'mightn', 'she', 'that', 'there', "shan't", 'below', 'again', 'shouldn', 'haven', 'we', 'he', 'been', 'itself', 'their', 'between', "won't", 'her', 'into', 'of', 'hers', "didn't", 'off', 'further', "that'll", 'did', 'than', 'll', 'me', "isn't", 'had', 'whom', 'it', 'have', "you'll", 'then', 'before', 'ours', 'down', 'all', 'yourselves', 'y', 'after', 'or', 'his', 'out', 'why', 'if', 'i', 'ain', 'which', 'didn', 'should', 'won', 'how', 'on', 'other', 'as', 'him', 'shan', 'hadn', 'above', 'each', 'until', 'for', 'needn', 'o', 'aren', 'they', "you've", 'doing', 'from', 'isn', 'himself', 'weren', "hadn't", 'its', 'this', 'myself', 'will', 'in', 'yours', 'through', "shouldn't", 'such', 'has', 'and', "mightn't", 'be', 'wouldn', 'was', "mustn't", 'couldn', 'ourselves', 'here', 've', 'own', 'while', 'once', "doesn't", "it's", 'theirs', 'doesn', 'are', 'those', 'm', 'these', 'any', 'no', 't', 'themselves', 'does', 'during', 'just', "wasn't", 'by', 'a', 'nor', 'my', 'about', 'too', 'against', "you'd", 'both', 'ma', 'but', 'up', 'when', 'your'}
-        Stopwords = set(stop)
+        Stopwords = set(stopwords.words('english'))
         word_freq = {}
         for word in word_tokenize(text_):
             if word not in Stopwords:
@@ -92,7 +93,7 @@ def classify():
             per = round((pred[0][0])*100,2) 
         Data = {'Title':title, 'Author':author,"Date_created":Date_created,
             'Time_Created':Time_Created, 'Summmary' : summary,
-            'Last_Updated':Last_Updated, 'Lime_Updated':Lime_Updated,
+            'Last_Updated':Last_Updated, 'Time_Updated':Time_Updated,
             'Sentiment' : result, 'Certainity_Percentage': per, 'Contnt':content}
     elif (code == 401):
         Data ={"Code:":code, "message":"Unauthorized"}
@@ -100,6 +101,5 @@ def classify():
     else: 
         Data ={"Code:":code, "message":"Not found"}
     return jsonify(Data)
-
 if __name__ =='__main__':
      app.run(port = 800)
